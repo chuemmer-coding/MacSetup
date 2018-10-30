5c7c6db9e2e807265ff7c80505ded7214fa6eacd
Colin Huemmer <chuemmer13@gmail.com>
2018-10-30 01:43:53 -0400

    Should open pokemon terminal to allow for setting env path to Python3 now, also made edits to init.el that were preventing challenger deep from properly loading. Opam version still not at 4.04.0, stuck in 4.07.0, currently working on a fix

--- mac_setup.py
+++ mac_setup.py
@@ -1,8 +1,8 @@
-#!usr/bin/python
+#!/usr/bin/python
 
 import os
 
-setup = input("Press 1 for just school setup, 2 for just personal, 3 for both\n")
+setup = input("Press 1 for just school setup, 2 for personal and school")
 
 if (setup == 1 or setup == 2):
 #class setup
@@ -11,6 +11,8 @@ if (setup == 1 or setup == 2):
     os.system("brew install ocaml opam rust graphviz")
 
     os.system("sudo gem install sqlite3 sinatra")
+
+    os.system("opam init")
 
     os.system("ocaml -version")
 
@@ -21,17 +23,19 @@ if (setup == 1 or setup == 2):
         os.system("eval `opam config env`")
 
         os.system("opam init")
-        os.system("source ~/.bash_profile")
-        os.system("opam install ocamlfind ounit utop dune")
+        os.system("source ~/.bash_profile -y")
+        os.system("opam install ocamlfind ounit utop dune -y")
 
 if setup == 2:
     os.system("brew install git")
     os.system("brew cask install iterm2")
     os.system("brew install node")
     os.system("npm install --global pokemon-terminal")
+    os.system("brew install wget")
+    os.system("vim /usr/local/pokemon")
     os.system("brew cask install emacs")
     os.system("wget ter.ps/chumsetup -O init.el")
-    os.system("mv init.el ~/emacs.d/")
+    os.system("mv init.el ~/.emacs.d/")
     os.system("brew cask install google-chrome")
     os.system("brew cask install marshallofsound-google-play-music-player")
     os.system("git clone https://github.com/chuemmer-coding/MacSetup.git")
